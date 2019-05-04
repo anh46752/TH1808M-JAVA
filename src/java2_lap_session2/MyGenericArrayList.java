63192bb58d5bc4e41e0840576e434a693f70269b
tuananh <anhntth1807061@gmail.com>
2019-05-04 21:47:58 +0700

    btt

---
+++ src/java2_lap_session2/MyGenericArrayList.java
@@ -0,0 +1,13 @@
+package java2_lap_session2;
+
+import java.util.ArrayList;
+
+public class MyGenericArrayList<N> {
+
+    public ArrayList<N> arrayNumber;
+
+    public MyGenericArrayList(){
+        this.arrayNumber = new ArrayList<>();
+    }
+
+}
